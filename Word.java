210b1b24979bbde68de5ce4886b0c11d920f4f40
大森 大 <>
2018-03-25 13:16:39 +0900

    一旦現状を提出いたします。

---
+++ Word.java
@@ -0,0 +1,31 @@
+public class Word {
+    private String word;
+    private String meaning;
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
